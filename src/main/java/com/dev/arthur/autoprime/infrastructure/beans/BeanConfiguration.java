aa6beed50e63cfab8870ee3115ff775f8cc32c36
arthurvasc-hub <arthurvasc2@gmail.com>
2025-06-24 13:28:16 -0300

    chore(config): wire user dependencies in BeanConfiguration

--- src/main/java/com/dev/arthur/autoprime/infrastructure/beans/BeanConfiguration.java
+++ src/main/java/com/dev/arthur/autoprime/infrastructure/beans/BeanConfiguration.java
@@ -1,4 +1,17 @@
 package com.dev.arthur.autoprime.infrastructure.beans;
 
+import com.dev.arthur.autoprime.domain.entities.AppUser;
+import com.dev.arthur.autoprime.domain.gateway.UserGateway;
+import com.dev.arthur.autoprime.domain.useCases.CreateUserCase;
+import com.dev.arthur.autoprime.domain.useCases.CreateUserCaseImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class BeanConfiguration {
+
+    @Bean
+    public CreateUserCase createUser(UserGateway userGateway){
+        return new CreateUserCaseImpl(userGateway);
+    }
 }

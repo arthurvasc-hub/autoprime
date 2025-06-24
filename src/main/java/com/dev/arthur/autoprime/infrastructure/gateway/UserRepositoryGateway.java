b51e183457803d477f4757743594ff5eff050a72
arthurvasc-hub <arthurvasc2@gmail.com>
2025-06-24 13:28:14 -0300

    feat(adapter): implement UserGateway using JPA repository

--- src/main/java/com/dev/arthur/autoprime/infrastructure/gateway/UserRepositoryGateway.java
+++ src/main/java/com/dev/arthur/autoprime/infrastructure/gateway/UserRepositoryGateway.java
@@ -2,12 +2,31 @@ package com.dev.arthur.autoprime.infrastructure.gateway;
 
 import com.dev.arthur.autoprime.domain.entities.AppUser;
 import com.dev.arthur.autoprime.domain.gateway.UserGateway;
-import org.apache.catalina.User;
+import com.dev.arthur.autoprime.infrastructure.mapper.UserEntityMapper;
+import com.dev.arthur.autoprime.infrastructure.persistence.UserEntity;
+import com.dev.arthur.autoprime.infrastructure.persistence.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
 
+@Component
+@RequiredArgsConstructor
 public class UserRepositoryGateway implements UserGateway {
 
+    private UserRepository userRepository;
+    private UserEntityMapper userEntityMapper;
+
     @Override
-    public User saveUser(AppUser user) {
-        return null;
+    public AppUser saveUser(AppUser appUser) {
+        AppUser user = new AppUser(
+                appUser.id(),
+                appUser.name(),
+                appUser.email(),
+                appUser.password(),
+                appUser.userType(),
+                appUser.gender()
+        );
+
+        UserEntity save = userEntityMapper.domainToEntity(user);
+        return userEntityMapper.entityToDomain(save);
     }
 }

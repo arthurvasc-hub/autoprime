bb2a0cbfce0044c2594989036f8280ad78515ff6
arthurvasc-hub <arthurvasc2@gmail.com>
2025-06-24 13:28:06 -0300

    feat(infrastructure): add JPA entity and repository for User

--- src/main/java/com/dev/arthur/autoprime/infrastructure/persistence/UserEntity.java
+++ src/main/java/com/dev/arthur/autoprime/infrastructure/persistence/UserEntity.java
@@ -1,4 +1,28 @@
 package com.dev.arthur.autoprime.infrastructure.persistence;
 
+import com.dev.arthur.autoprime.domain.enums.Gender;
+import com.dev.arthur.autoprime.domain.enums.UserType;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
 public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+    private String password;
+    private UserType userType;
+    private Gender gender;
+
+
 }
--- src/main/java/com/dev/arthur/autoprime/infrastructure/persistence/UserRepository.java
+++ src/main/java/com/dev/arthur/autoprime/infrastructure/persistence/UserRepository.java
@@ -1,4 +1,8 @@
 package com.dev.arthur.autoprime.infrastructure.persistence;
 
-public class UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
 }

09577bc8f9d63507a73f7eabb02ff169b4dcbbba
arthurvasc-hub <arthurvasc2@gmail.com>
2025-06-25 19:19:08 -0300

    feat(user): integrate token generation with user controller for testing

--- src/main/java/com/dev/arthur/autoprime/infrastructure/presentation/UserController.java
+++ src/main/java/com/dev/arthur/autoprime/infrastructure/presentation/UserController.java
@@ -6,6 +6,7 @@ import com.dev.arthur.autoprime.infrastructure.mapper.UserRequestMapper;
 import com.dev.arthur.autoprime.infrastructure.mapper.UserResponseMapper;
 import com.dev.arthur.autoprime.infrastructure.request.SignupRequest;
 import com.dev.arthur.autoprime.infrastructure.response.UserResponse;
+import com.dev.arthur.autoprime.infrastructure.security.JwtUtil;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -20,16 +21,17 @@ public class UserController {
     private final CreateUserCase createUserCase;
     private final UserRequestMapper userRequestMapper;
     private final UserResponseMapper userResponseMapper;
+    private final JwtUtil jwt;
+
+
 
 
 
     @PostMapping("/register")
     public ResponseEntity<UserResponse> newUser(@RequestBody @Valid SignupRequest signupRequest) {
         AppUser appUser = createUserCase.execute(userRequestMapper.requestToDomain(signupRequest));
-
+        jwt.generateToken(appUser);
         UserResponse response = userResponseMapper.domainToResponse(appUser);
-
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
-
 }

dbae84c642e7ca992e25546aeafe720144b3a38c
arthurvasc-hub <arthurvasc2@gmail.com>
2025-06-25 19:19:07 -0300

    feat(security): implement JWT token generation and validation

---
+++ src/main/java/com/dev/arthur/autoprime/infrastructure/security/JwtUtil.java
@@ -0,0 +1,70 @@
+package com.dev.arthur.autoprime.infrastructure.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.dev.arthur.autoprime.domain.entities.AppUser;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @Value("${jwt.expiration}")
+    private long expirationMillis;
+
+    public String generateToken(AppUser user) {
+
+        Algorithm algorithm = Algorithm.HMAC256(secret);
+
+        return JWT.create()
+                .withSubject(user.id().toString())
+                .withClaim("type", user.userType().name())
+                .withIssuedAt(new Date())
+                .withExpiresAt(new Date(System.currentTimeMillis() + expirationMillis))
+                .sign(algorithm);
+    }
+
+    public Boolean validateToken(String token) {
+
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            JWTVerifier jwtVerifier = JWT.require(algorithm).build();
+            jwtVerifier.verify(token);
+
+            return true;
+
+        } catch (JWTVerificationException e) {
+            return false;
+        }
+    }
+
+    public String extractUserId(String token) {
+        DecodedJWT decodedJWT = JWT.decode(token);
+        return decodedJWT.getSubject();
+    }
+
+    public String extractUserType(String token) {
+        DecodedJWT decodedJWT = JWT.decode(token);
+        return decodedJWT.getClaim("type").asString();
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+}
+

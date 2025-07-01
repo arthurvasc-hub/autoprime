f7f0b8fec773c468c0be82aa75522a84c7ffef2c
arthurvasc-hub <arthurvasc2@gmail.com>
2025-07-01 19:48:13 -0300

    Update UserRepository interface if necessary for email search

--- src/main/java/com/dev/arthur/autoprime/infrastructure/persistence/UserRepository.java
+++ src/main/java/com/dev/arthur/autoprime/infrastructure/persistence/UserRepository.java
@@ -3,6 +3,9 @@ package com.dev.arthur.autoprime.infrastructure.persistence;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    Optional<UserEntity> findByEmail(String email);
 }

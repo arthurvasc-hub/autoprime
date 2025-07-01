3caeedd2350e2d0b7257b40affeafcda97618e48
arthurvasc-hub <arthurvasc2@gmail.com>
2025-07-01 19:48:04 -0300

    Implement FindUserCase for retrieving user by email

--- src/main/java/com/dev/arthur/autoprime/domain/useCases/FindUserCase.java
+++ src/main/java/com/dev/arthur/autoprime/domain/useCases/FindUserCase.java
@@ -1,4 +1,8 @@
 package com.dev.arthur.autoprime.domain.useCases;
 
+import com.dev.arthur.autoprime.domain.entities.AppUser;
+
 public interface FindUserCase {
+
+    public AppUser execute(String email);
 }
--- src/main/java/com/dev/arthur/autoprime/domain/useCases/FindUserCaseImpl.java
+++ src/main/java/com/dev/arthur/autoprime/domain/useCases/FindUserCaseImpl.java
@@ -1,4 +1,18 @@
 package com.dev.arthur.autoprime.domain.useCases;
 
-public class FindUserCaseImpl {
+import com.dev.arthur.autoprime.domain.entities.AppUser;
+import com.dev.arthur.autoprime.domain.gateway.UserGateway;
+
+public class FindUserCaseImpl implements FindUserCase {
+
+    private UserGateway userGateway;
+
+    public FindUserCaseImpl(UserGateway userGateway) {
+        this.userGateway = userGateway;
+    }
+
+    @Override
+    public AppUser execute(String email) {
+        return userGateway.findUserByEmail(email);
+    }
 }

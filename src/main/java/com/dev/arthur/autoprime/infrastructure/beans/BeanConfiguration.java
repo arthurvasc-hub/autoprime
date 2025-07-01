9d16a53be4b1eba90628a8a5b2fe1fc13ea94dfc
arthurvasc-hub <arthurvasc2@gmail.com>
2025-07-01 19:48:06 -0300

    Configure beans for PasswordEncoderGateway and use cases in BeanConfiguration

--- src/main/java/com/dev/arthur/autoprime/infrastructure/beans/BeanConfiguration.java
+++ src/main/java/com/dev/arthur/autoprime/infrastructure/beans/BeanConfiguration.java
@@ -1,9 +1,13 @@
 package com.dev.arthur.autoprime.infrastructure.beans;
 
 import com.dev.arthur.autoprime.domain.entities.AppUser;
+import com.dev.arthur.autoprime.domain.gateway.PasswordEncoderGateway;
 import com.dev.arthur.autoprime.domain.gateway.UserGateway;
 import com.dev.arthur.autoprime.domain.useCases.CreateUserCase;
 import com.dev.arthur.autoprime.domain.useCases.CreateUserCaseImpl;
+import com.dev.arthur.autoprime.domain.useCases.FindUserCase;
+import com.dev.arthur.autoprime.domain.useCases.FindUserCaseImpl;
+import org.apache.catalina.User;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,7 +15,10 @@ import org.springframework.context.annotation.Configuration;
 public class BeanConfiguration {
 
     @Bean
-    public CreateUserCase createUser(UserGateway userGateway){
-        return new CreateUserCaseImpl(userGateway);
+    public CreateUserCase createUser(UserGateway userGateway, PasswordEncoderGateway passwordEncoderGateway){
+        return new CreateUserCaseImpl(userGateway, passwordEncoderGateway);
     }
+
+    @Bean
+    public FindUserCase findUser(UserGateway userGateway) {return new FindUserCaseImpl(userGateway); }
 }

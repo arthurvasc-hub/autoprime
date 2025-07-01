0f0881b3bd7b3fc5b16c73beab20de890a5cce9c
arthurvasc-hub <arthurvasc2@gmail.com>
2025-07-01 19:48:09 -0300

    Implement PasswordEncoderGateway using Spring's BCryptPasswordEncoder

--- src/main/java/com/dev/arthur/autoprime/infrastructure/gateway/BcryptPasswordEncoder.java
+++ src/main/java/com/dev/arthur/autoprime/infrastructure/gateway/BcryptPasswordEncoder.java
@@ -1,4 +1,25 @@
 package com.dev.arthur.autoprime.infrastructure.gateway;
 
-public class BcryptPasswordEncoder {
+import com.dev.arthur.autoprime.domain.gateway.PasswordEncoderGateway;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BcryptPasswordEncoder implements PasswordEncoderGateway {
+
+    private final PasswordEncoder passwordEncoder;
+
+    public BcryptPasswordEncoder(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public String encode(String rawPassword) {
+        return passwordEncoder.encode(rawPassword);
+    }
+
+    @Override
+    public boolean matches(String rawPassword, String encodedPassword) {
+        return passwordEncoder.matches(rawPassword, encodedPassword);
+    }
 }

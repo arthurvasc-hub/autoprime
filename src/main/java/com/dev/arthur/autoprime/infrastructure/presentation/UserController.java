82f357a38aff2d3509e09578c258adc1884beebb
arthurvasc-hub <arthurvasc2@gmail.com>
2025-07-01 19:48:15 -0300

    Start login implementation in UserController using password comparison

--- src/main/java/com/dev/arthur/autoprime/infrastructure/presentation/UserController.java
+++ src/main/java/com/dev/arthur/autoprime/infrastructure/presentation/UserController.java
@@ -2,8 +2,10 @@ package com.dev.arthur.autoprime.infrastructure.presentation;
 
 import com.dev.arthur.autoprime.domain.entities.AppUser;
 import com.dev.arthur.autoprime.domain.useCases.CreateUserCase;
+import com.dev.arthur.autoprime.domain.useCases.FindUserCase;
 import com.dev.arthur.autoprime.infrastructure.mapper.UserRequestMapper;
 import com.dev.arthur.autoprime.infrastructure.mapper.UserResponseMapper;
+import com.dev.arthur.autoprime.infrastructure.request.LoginRequest;
 import com.dev.arthur.autoprime.infrastructure.request.SignupRequest;
 import com.dev.arthur.autoprime.infrastructure.response.UserResponse;
 import com.dev.arthur.autoprime.infrastructure.security.JwtUtil;
@@ -13,21 +15,19 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Objects;
+
 @RestController
 @RequestMapping("api/v1")
-
+@RequiredArgsConstructor
 public class UserController {
 
     private final CreateUserCase createUserCase;
+    private final FindUserCase findUserCase;
     private final UserRequestMapper userRequestMapper;
     private final UserResponseMapper userResponseMapper;
+    private final JwtUtil jwtUtil;
 
-    public UserController(CreateUserCase createUserCase, UserRequestMapper userRequestMapper, UserResponseMapper userResponseMapper, JwtUtil jwt) {
-        this.createUserCase = createUserCase;
-        this.userRequestMapper = userRequestMapper;
-        this.userResponseMapper = userResponseMapper;
-
-    }
 
     @PostMapping("/register")
     public ResponseEntity<UserResponse> newUser(@RequestBody @Valid SignupRequest signupRequest) {
@@ -35,4 +35,14 @@ public class UserController {
         UserResponse response = userResponseMapper.domainToResponse(appUser);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
+
+    @PostMapping("/auth/login")
+    public ResponseEntity<String> loginUser(@RequestBody @Valid LoginRequest login) {
+        AppUser foundUser = findUserCase.execute(login.email());
+        if(!Objects.equals(foundUser.email(), login.email()) || !Objects.equals(foundUser.password(), login.password())) {
+            jwtUtil.generateToken(foundUser);
+
+        }
+    }
+
 }

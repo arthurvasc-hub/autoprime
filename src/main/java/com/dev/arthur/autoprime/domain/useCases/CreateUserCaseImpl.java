2f0a80f0ecd927dfe6f1485cf6a1aa6a38dbade8
arthurvasc-hub <arthurvasc2@gmail.com>
2025-07-01 19:47:55 -0300

    Encode password inside CreateUserCaseImpl to keep infrastructure independent

--- src/main/java/com/dev/arthur/autoprime/domain/useCases/CreateUserCaseImpl.java
+++ src/main/java/com/dev/arthur/autoprime/domain/useCases/CreateUserCaseImpl.java
@@ -1,18 +1,29 @@
 package com.dev.arthur.autoprime.domain.useCases;
 
 import com.dev.arthur.autoprime.domain.entities.AppUser;
+import com.dev.arthur.autoprime.domain.gateway.PasswordEncoderGateway;
 import com.dev.arthur.autoprime.domain.gateway.UserGateway;
 
 public class CreateUserCaseImpl implements CreateUserCase {
 
     private final UserGateway userGateway;
+    private final PasswordEncoderGateway passwordEncoder;
 
-    public CreateUserCaseImpl(UserGateway userGateway) {
+    public CreateUserCaseImpl(UserGateway userGateway, PasswordEncoderGateway passwordEncoder) {
         this.userGateway = userGateway;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
     public AppUser execute(AppUser user) {
-        return userGateway.saveUser(user);
+        AppUser userToSave = new AppUser(
+                user.id(),
+                user.name(),
+                user.email(),
+                passwordEncoder.encode(user.password()),
+                user.userType(),
+                user.gender()
+        );
+        return userGateway.saveUser(userToSave);
     }
 }
